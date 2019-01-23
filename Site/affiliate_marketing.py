3c18024877b6fde4e327e499210f2fa764eb3679
Tom W Hartung <tomwhartung@gmail.com>
2019-01-23 16:07:15 -0700

    Set values for the remaining links in affiliate_marketing.py .

--- Site/affiliate_marketing.py
+++ Site/affiliate_marketing.py
@@ -23,6 +23,11 @@ class AffiliateLinks:
         'psychological_types': '',
         'point_and_line': '',
         'gifts_differing': '',
+        'understand_1': '',
+        'new_art_life': '',
+        'understand_spanish': '',
+        'understand_2': '',
+        'presidents': '',
     }
 
     #
@@ -40,5 +45,8 @@ class AffiliateLinks:
         self.afl_button['psychological_types'] = afl_none['psychological_types']
         self.afl_button['point_and_line'] = afl_none['point_and_line']
         self.afl_button['gifts_differing'] = afl_none['gifts_differing']
-
-
+        self.afl_button['understand_1'] = afl_none['understand_1']
+        self.afl_button['new_art_life'] = afl_none['new_art_life']
+        self.afl_button['understand_spanish'] = afl_none['understand_spanish']
+        self.afl_button['understand_2'] = afl_none['understand_2']
+        self.afl_button['presidents'] = afl_none['presidents']
